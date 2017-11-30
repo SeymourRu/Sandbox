fe37f0577595b0451ad423e857f02e3e3f7cea19
Seymour <ilyamalinaya@mail.ru>
2017-12-01 02:06:55 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vectrics;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vectrics.Vector2D val = new Vector2D();
+        }
+    }
+}
